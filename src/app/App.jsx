eef6a0dd8d9f5712f292ca92affc05e603db82ef
moneyza001 <supalap.ss@gmail.com>
2023-08-30 11:54:13 +0700

    Edit sidebar menu

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import {FaCalendar, FaCalendarAlt, FaHome,FaInbox} from 'react-icons/fa';
 import Header from "../components/Header.jsx";
 function App() {
   return (
@@ -7,7 +8,27 @@ function App() {
         <div className="todo__header">
           <Header />
         </div>
-        <div className="todo__sidebar">SideBar</div>
+        <div className="todo__sidebar">
+          <aside className="sidebar">
+            <section className="sidebar__category">
+              <ul className="list">
+                  <li className="list__item">
+                      <FaInbox className="list__item__icon"/>
+                    <p className="list__item__text">Inbox</p>
+                  </li>
+                  <li className="list__item">
+                      <FaCalendar className="list__item__icon"/>
+                    <p className="list__item__text">Today</p>
+                  </li>
+                  <li className="list__item">
+                      <FaCalendarAlt className="list__item__icon"/>
+                    <p className="list__item__text">Next 7 days</p>
+                  </li>
+              </ul>
+            </section>
+            <section className="sidebar__category">2</section>
+          </aside>
+        </div>
         <div className="todo__content">TodoContent</div>
       </div>
     </div>
