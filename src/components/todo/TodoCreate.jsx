fcde66da6cd0cd7438de8a9f732ab36fb3a01ffa
moneyza001 <supalap.ss@gmail.com>
2023-08-31 11:03:34 +0700

    State condition render in addTask in todoCreate

--- src/components/todo/TodoCreate.jsx
+++ src/components/todo/TodoCreate.jsx
@@ -1,15 +1,50 @@
-import styles from './TodoCreate.module.scss';
-import { FaPlus } from 'react-icons/fa';
-import { HiPlus } from 'react-icons/hi';
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { HiPlus } from "react-icons/hi";
+
+import TodoForm from "./TodoForm";
+import styles from "./TodoCreate.module.scss";
+/*
+#1 -Condition Redering
+-Default : Show Button & Text
+-Active : Show TodoForm
+
+concept : true ? <AddTask/> : <TodoForm/>
+*/
+
+/*
+#2 Event handling
+เอาฟังก์ชั่นไปผูกติด UI เพื่อให้ user เลือกใช้เอง
+*/
+
+/*
+#3 JS Value ไม่สามารถทำให้ React re-render ได้
+ต้องใช้ State
+*/
+
+/*
+#4 React State : React Hook
+const [state,setState] = useState(innitialState : any)
+*/
 
 function TodoCreate() {
+  const [isOpenForm, setIsOpenForm] = useState(false);
+  const handleClick = function (event) {
+    setIsOpenForm(!isOpenForm);
+  };
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+        <TodoForm />
+      ) : (
+        <div className={styles.todo__create} onClick={handleClick}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/todo/TodoLists.jsx
+++ src/components/todo/TodoLists.jsx
@@ -1,15 +1,19 @@
-import styles from './TodoLists.module.scss';
-import { FaTrashAlt, FaPen } from 'react-icons/fa';
-import { HiOutlineCheck } from 'react-icons/hi';
+import styles from "./TodoLists.module.scss";
+import { FaTrashAlt, FaPen } from "react-icons/fa";
+import { HiOutlineCheck } from "react-icons/hi";
 
 function TodoLists() {
   return (
     <ul className={styles.todo__lists}>
       <li className={styles.todo}>
-        <div className={styles.todo__checkbox}>
-          <HiOutlineCheck className={styles.todo__checkbox__icon} />
+        <div
+          className={`${styles.todo__checkbox} ${styles.todo__checkbox__done}`}
+        >
+          <HiOutlineCheck className={`styles.todo__checkbox__icon`} />
         </div>
-        <p className={styles.todo__task}>todo-item 1 </p>
+        <p className={`${styles.todo__task} ${styles.todo__task__done}`}>
+          todo-item 1{" "}
+        </p>
         <span className={styles.todo__date}>30 Aug</span>
         <div className={styles.todo__action}>
           <span>
