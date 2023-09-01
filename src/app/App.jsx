f454d6cd3ded53d2836b1e18ba161077d61703c9
moneyza001 <supalap.ss@gmail.com>
2023-09-01 10:54:02 +0700

    Edit file name from todo to Todo

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,23 +1,23 @@
 // Dependencies
-import './App.scss';
-import AppBar from '../components/Common/AppBar/AppBar';
-import SideBar from '../components/SideBar/SideBar';
-import TodoHeader from '../components/Todo/TodoHeader';
-import TodoCreate from '../components/Todo/TodoCreate';
-import TodoLists from '../components/Todo/TodoLists';
-import { Button } from '../components/Common/Button/Button';
+import "./App.scss";
+import AppBar from "../components/Common/AppBar/AppBar";
+import SideBar from "../components/SideBar/SideBar";
+import TodoHeader from "../components/Todo/TodoHeader";
+import TodoCreate from "../components/Todo/TodoCreate";
+import TodoLists from "../components/Todo/TodoLists";
+import { Button } from "../components/Common/Button/Button";
 
 function App() {
   return (
-    <div className='todo'>
-      <div className='todo__header'>
+    <div className="todo">
+      <div className="todo__header">
         <AppBar />
       </div>
-      <div className='todo__sidebar'>
+      <div className="todo__sidebar">
         <SideBar />
       </div>
-      <div className='todo__content'>
-        <main className='todo__container'>
+      <div className="todo__content">
+        <main className="todo__container">
           <TodoHeader />
           <TodoCreate />
           <TodoLists />
